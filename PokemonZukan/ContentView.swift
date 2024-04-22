8aa673a991ef47adf1566dcc585eee521f92ac09
Keisuke Isaji <keisuke_isaji@mail.toyota.co.jp>
2024-04-22 13:26:43 +0900

    Initial Commit

---
+++ PokemonZukan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokemonZukan
+//
+//  Created by 伊佐治恵介 on 2024/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokemonZukan/PokemonZukanApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonZukanApp.swift
+//  PokemonZukan
+//
+//  Created by 伊佐治恵介 on 2024/04/22.
+//
+
+import SwiftUI
+
+@main
+struct PokemonZukanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
